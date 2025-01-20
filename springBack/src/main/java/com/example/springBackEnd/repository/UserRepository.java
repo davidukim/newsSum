32f8a446c2ed5e174fc3ae3c9782dcc030310e0f
davidkim <can.davidkim@gmail.com>
2025-01-20 15:00:20 -0500

    Several entities and repository classes for database interaction purpose

---
+++ springBack/src/main/java/com/example/springBackEnd/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.springBackEnd.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+public class UserRepository {
+    
+}
--- springBack/src/main/java/com/example/springBackEnd/service/NewsService.java
+++ springBack/src/main/java/com/example/springBackEnd/service/NewsService.java
@@ -1,8 +1,17 @@
 package com.example.springBackEnd.service;
 
+import com.example.springBackEnd.model.NewsResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NewsService {
-    
+    @Autowired
+    private NewsResponse response;
+
+    public NewsResponse fetchNews(){
+
+        return response;
+    }
 }
